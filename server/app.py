d71dadf1bd0ade5d7660266f97d793b4c4dbabd1
Joseph <j.mburu.pro@gmail.com>
2025-06-26 12:02:21 +0300

    restful crud

--- server/app.py
+++ server/app.py
@@ -16,7 +16,14 @@ db.init_app(app)
 
 api = Api(app)
 
+class Home(Resource):
+    
+    def get(self):
+        
+       response = make_response(jsonify({"message": "Welcome to the Plant API!"}), 200)
+       return response
 
+api.add_resource(Home, '/')
 class Plants(Resource):
 
     def get(self):
@@ -46,8 +53,38 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        data = request.get_json()
+        plant = Plant.query.filter_by(id = id).first()
 
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
 
+        if 'is_in_stock' in data:
+            plant.is_in_stock = data['is_in_stock']
+
+        db.session.commit()
+        return {
+            "id": plant.id,
+            "name": plant.name,
+            "image": plant.image,
+            "price": plant.price,
+            "is_in_stock": plant.is_in_stock
+        }, 200
+    
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return "", 204
+    
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
